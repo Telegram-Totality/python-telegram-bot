3e6934da673c13788e97d656025ae4bf92b17612
Evert <evert@hatching.io>
2020-08-14 19:28:10 +0200

    Update call uploads

--- telegram/totality/inlinetotalitybutton.py
+++ telegram/totality/inlinetotalitybutton.py
@@ -40,9 +40,11 @@ class InlineTotalityButton(InlineKeyboardButton):
 
     def upload_call(self):
         endpoint = os.environ.get("TOTALITY_ENDPOINT")
+        if not endpoint:
+            raise ValueError("TOTALITY_ENDPOINT not set")
         digest = Hash(SHA224(), backend=default_backend())
         digest.update(str.encode(self.data))
-        digest.update(str.encode(str(datetime.datetime.utcnow().microsecond)))
+        digest.update(str.encode(str(datetime.datetime.utcnow())))
         self.secret_hash = digest.finalize().hex()
         
         http = urllib3.PoolManager()
